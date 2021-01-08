d83860dee24a90f02598b47d7480d5d5363cc678
Kirtesh Patel <kirteshpatel@v1041-wn-rt-d-168-244.campus-dynamic.uwaterloo.ca>
2021-01-08 18:55:03 -0500

    new config

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyATb_EE2gwFuww5Fbxx4bTJAnzUQcSPxJc",
-    authDomain: "tinder-31def.firebaseapp.com",
-    databaseURL: "https://tinder-31def.firebaseio.com",
-    projectId: "tinder-31def",
-    storageBucket: "tinder-31def.appspot.com",
-    messagingSenderId: "339132347279",
-    appId: "1:339132347279:web:17a0b08683bda229b751fa",
-    measurementId: "G-GTXVJ384Y0"
-  };
+  apiKey: "AIzaSyCBvIBA5nkUVwDc5pNqnd8MJYJ4b2ufe6M",
+  authDomain: "fumble-cbe9f.firebaseapp.com",
+  projectId: "fumble-cbe9f",
+  storageBucket: "fumble-cbe9f.appspot.com",
+  messagingSenderId: "1073924619018",
+  appId: "1:1073924619018:web:61c9bd93625a84fc6c18f3",
+  measurementId: "G-72NLT6R19C"
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const database = firebaseApp.firestore();
